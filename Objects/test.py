67ae01968442e67af00c2ff1e963e145757efe55
AVasilyev1998 <avasilyev1998@mail.ru>
2019-11-02 17:36:54 +0300

    FoodShop in work

--- Objects/test.py
+++ Objects/test.py
@@ -1,5 +1,17 @@
 from Objects.film import Film
 
+#
+# film = Film('name', '1', '2')
+# print(film)
 
-film = Film('name', '1', '2')
-print(film)
+#  queue test for food_shop and ticket_office
+from collections import deque
+
+q = deque()
+
+q.append(1)
+q.append(2)
+print(q)
+
+print(q.popleft())
+print(q.popleft())
