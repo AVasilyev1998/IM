eaea2c7e6c07334c7238f685422e842906e7db50
dmfananddu <af0706@yandex.ru>
2019-11-02 18:42:24 +0300

    second artyom commit

--- Objects/ticket_office.py
+++ Objects/ticket_office.py
@@ -5,21 +5,27 @@ from Objects.film import Film
 class Ticket(object):
     """
     Билет
-    - фильм
+    - фильм (по названию)
     - цена билета
     """
-    def __init__(self, ticket_film: Film, price):
-        self.film = ticket_film
+    def __init__(self, ticket_hall: CinemaHall, price):
+        # self.id = hash()
+        try:
+            self.hall = ticket_hall
+        except Exception('Билет без сеанса!') as e:
+            print(e)
+        # Принтуем ошибку, если у билета не оказалось сеанса.
         self.price = price
 
     def __repr__(self):
-        return f'ticket to {self.film.name} with price {self.price}'
+        return f'ticket to {self.hall.next_film_name} with price {self.price}'
 
 
 class TicketOffice(object):
     """
     Касса
-    - билеты
+    - id сотрудника, который сейчас работает (добавить)
+    - билеты, которые продала касса, просто накопитель - стата будущая
     """
     def __init__(self, tickets: list):
         self.tickets = tickets
@@ -29,8 +35,7 @@ class TicketOffice(object):
 
 
 if __name__ == '__main__':
-    time = datetime.datetime.now()
-    film = Film('33 разбойника', time.strftime('%D'), '1:20')
+    film = Film('33 разбойника', datetime.datetime.now().strftime('%d/%m/%y'), '1:20')
     print(film)
     ticket = Ticket(film, 120)
     print(ticket)
