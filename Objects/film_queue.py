073557774a510fcf9bada79070e6f1a14023a718
AVasilyev1998 <avasilyev1998@mail.ru>
2019-11-19 12:29:36 +0300

    added film_queue

--- Objects/film_queue.py
+++ Objects/film_queue.py
@@ -1,4 +1,6 @@
 from collections import deque
+from Objects.film_creator import FilmCreator
+from Objects.film import Film
 
 
 class FilmQueue:
@@ -6,6 +8,31 @@ class FilmQueue:
         Очередь фильмов
     """
     def __init__(self):
-        pass
+        self.__deque = deque()
+
+    def add_film(self, film: Film):
+        self.__deque.append(film)
+
+    def get_film(self) -> Film:
+        return self.__deque.popleft()
+
+    def __repr__(self):
+        return f'{self.__deque}'
 
 
+if __name__ == "__main__":
+    films = []
+    with open('Objects/films.txt', 'r') as reader:
+        for film in reader:
+            tmp_film = film.strip('\n')
+            films.append(tmp_film)
+    creator = FilmCreator(names=films)
+
+    film_queue = FilmQueue()
+    film_queue.add_film(creator.create_film())
+    film_queue.add_film(creator.create_film())
+    print(film_queue)
+    print(film_queue.get_film())
+    print(film_queue)
+    film_queue.add_film(creator.create_film())
+    print(film_queue)
