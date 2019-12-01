f8abf82de4131e9f5034a41b96d8802f3c43bfbf
AVasilyev1998 <avasilyev1998@mail.ru>
2019-12-01 16:02:51 +0300

    added some comments nothing else

--- Objects/client.py
+++ Objects/client.py
@@ -4,21 +4,23 @@ from utils.random_mod import random_with_chance
 class Client(object):
     """
     Клиент
-    - денег достаточно на все хотелки, остальное не важно
+    - денег достаточно на все потребности -> их не учитываем
     - предпочтения по фильмам (3 фильма) list[]
     - предпочтения по закускам -> boolean  1 or 0
     - предпочтения по напиткам -> boolean 1 or 0
     """
     def __init__(self, films):
-        # self.id = hash()
         if isinstance(films, list):
             self.films = films
         else:
             raise TypeError
+        
+        #  2 множитель ниже - продолжительность операции в условных еденицах
         self.food_preference = random_with_chance(30) * 0.4
         self.drink_preference = random_with_chance(40) * 0.6
-        self.id = str(self.__hash__())
-        self.statistics = {}
+        
+        self.id = str(self.__hash__())  # для идентификации клиентв
+        self.statistics = {}  # TODO: собирать статистику по клиенту сюда
 
     def __repr__(self):
         return f'client:{self.id}\n films:{self.films} \n food: {self.food_preference} \n drink: {self.drink_preference}\n'
