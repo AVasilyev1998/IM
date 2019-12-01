3618143177df0bb41f12b4bfb1f9a487a1100eb7
AVasilyev1998 <avasilyev1998@mail.ru>
2019-12-01 16:00:28 +0300

    CinemaHall optimized added opportunity to initialize hall manually

--- Objects/cinema_hall.py
+++ Objects/cinema_hall.py
@@ -1,8 +1,6 @@
 import datetime
 from random import choice
 
-from film import Film
-
 
 class CinemaHall(object):
     """
@@ -11,10 +9,17 @@ class CinemaHall(object):
     - вместительность
     """
 
-    def __init__(self):
-        self.name = ""
-        self.capacity = 0
-        self.last_film_end_time = datetime.datetime.combine(datetime.date.today(), datetime.time(7, 30))
+    def __init__(self, last_film_end_time = datetime.datetime.combine(datetime.date.today(),datetime.time(7, 30)),
+                 name=0, capacity=0):
+        if not name and not capacity:
+            self.last_film_end_time = last_film_end_time
+            self.create_hall()  # выбираем название и вместительность зала
+        elif name and capacity:
+            self.last_film_end_time = last_film_end_time
+            self.name = name
+            self.capacity = capacity
+        else:
+            raise TypeError
 
     def create_hall(self):
         halls_names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -28,5 +33,14 @@ class CinemaHall(object):
 
 
 if __name__ == '__main__':
-    print('CinemaHall')
+    hall = CinemaHall()
+    print(hall)
+
+    hall2 = CinemaHall(name='XL', capacity=200)
+    print(hall2)
+
+    try:
+        a = CinemaHall(name='XL', last_film_end_time=1)
+    except TypeError:
+        print('Возможен ввод только всех параметров сразу: TypeError')
 
