8859aed9e95f835ccab32dd19cd0af823f5be533
dmfananddu <af0706@yandex.ru>
2019-12-01 13:57:46 +0300

    Sixth proficient commit from Artyom. Sheduler finishedgit status!

--- Objects/scheduler.py
+++ Objects/scheduler.py
@@ -24,17 +24,15 @@ class Session:
         self.film_name = film.name
         self.hall_name = hall.name
         self.free_sits = hall.capacity
-        self.start_time = datetime.datetime.combine(datetime.date.today(), datetime.time(8, 0))
-        self.end_time = self.start_time + datetime.timedelta(minutes=self.film.duration) + datetime.timedelta(minutes=15)
-        self.ticket_price = gen_ticket_price()
-
-    def gen_ticket_price(self):
-        return int(random.random()*200 + 100) # генератор цены билета от 100 до 300
+        self.start_time = hall.last_film_end_time + datetime.timedelta(minutes=30)
+        self.end_time = hall.last_film_end_time + datetime.timedelta(minutes=film.duration) + datetime.timedelta(minutes=45)
+        self.ticket_price = int(random()*200 + 100) # генератор цены билета от 100 до 300
 
     def __repr__(self):
         # не нравится мне эта реализация, но лень менять, поэтому ок)))
-        return f'{self.film_name} | starts at: {self.start_time.strftime("%H:%M")} | ends at: {self.end_time.time().strftime("%H:%M")} |' \
-               f' price: {self.ticket_price}'
+        return f'hall: {self.hall_name} | ' \
+                f'film: {self.film_name} | starts at: {self.start_time.strftime("%H:%M")} | ends at: {self.end_time.time().strftime("%H:%M")} | ' \
+                f'price: {self.ticket_price}'
 
 
 class Schedule:
@@ -54,12 +52,62 @@ class Schedule:
     def __init__(self, f, h):
         # произвольное количество фильмов и кинозалов,
         # чтобы было, где смотреть "Крымский мост"
-        self.films_list = [FilmCreator().create_film() for i in range(f)]
-        self.halls_list = [CinemaHall().create_hall() for i in range(h)]
+        self.films_list = []
+        self.halls_list = []
+        for i in range(f):
+            self.films_list.append(FilmCreator().create_film())
+        for i in range(h):
+            self.halls_list.append(CinemaHall().create_hall())
 
-        self.queues = []
+        # время закрытия кинотеатра
+        self.close_time = datetime.datetime.combine(datetime.date.today(), datetime.time(2, 0)) + datetime.timedelta(hours=24)
+
+        self.queues = [] # список всех сеансов по всем залам
         for i in range(h):
             self.queues.append(deque())
+
+    def create_schedule(self):
+        fi = 0
+        hi = 0
+        tmp_film = FilmCreator().create_film()
+        f = len(self.films_list)
+        h = len(self.halls_list)
+        if f == 0 or h == 0:
+            return self
+
+        # заполняем расписание сеансами, пока не упремся в 2-00 след.дня
+        while True:
+
+            # выбираем следующий зал с минимальным временем окончания предыдущего фильма
+            min_t = self.halls_list[0].last_film_end_time
+            hi = 0
+            for i in range(h):
+                if self.halls_list[i].last_film_end_time < min_t:
+                    min_t = self.halls_list[i].last_film_end_time
+                    hi = i
+
+            # выбираем фильм для зала
+            # если выбор всех возможных фильмов продлевает жизнь кинотеатра за self.close_time,
+            # то мы заканчиваем формирование дневного расписания и выдаем его
+            tmp_film = self.films_list[fi]
+            fi += 1
+            if fi == f:
+                fi = 0
+
+            if self.halls_list[hi].last_film_end_time + datetime.timedelta(minutes=tmp_film.duration) + datetime.timedelta(minutes=45) > self.close_time:
+                for i in range(f):
+                    if self.halls_list[hi].last_film_end_time + datetime.timedelta(minutes=self.films_list[i].duration) + datetime.timedelta(minutes=45) < self.close_time:
+                        tmp_film = self.films_list[i]
+                else:
+                    return self
+
+            # формирование сеанса
+            tmp_session = Session(self.halls_list[hi], tmp_film)
+            self.queues[hi].append(tmp_session)
+            
+            # изменение времени окончания последнего фильма у соответствующего зала
+            self.halls_list[hi].last_film_end_time = tmp_session.end_time
+
 
     def __repr__(self):
         return f'сеансов в залах:\n s:{len(self.queue_s)} | m:{len(self.queue_m)} | l:{len(self.queue_l)}'
@@ -68,9 +116,16 @@ class Schedule:
 if __name__ == "__main__":
     films_count = 7
     halls_count = 3
+    
+    # наполнение тестового расписания фильмами и залами
     test_schedule = Schedule(films_count, halls_count)
-    for i in range(films_count):
-        print(test_schedule.films_list[i])
+
+    # составление расписания
+    test_schedule = test_schedule.create_schedule()
+
+    # вывод тестового расписания по залам
     for i in range(halls_count):
         print(test_schedule.halls_list[i])
-    print('Session-Schedule')
+        for j in range(len(test_schedule.queues[i])):
+            print(test_schedule.queues[i][j])
+
