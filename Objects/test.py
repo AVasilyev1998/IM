507abddfd9f2ac27e8ac40d547bed9bad04dddff
AVasilyev1998 <avasilyev1998@mail.ru>
2019-12-03 11:05:17 +0300

    ___

--- Objects/test.py
+++ Objects/test.py
@@ -1,17 +1,54 @@
-from Objects.film import Film
+import simpy
+from client import Client
+import random
 
-#
-# film = Film('name', '1', '2')
-# print(film)
 
-#  queue test for food_shop and ticket_office
-from collections import deque
+class ClientSim(object):
+    def __init__(self, env, name):
+        self.client = Client()
+        self.action = env.process(self.run())
+        self.env = env
+        self.name = random.randint(1000, 9999)
 
-q = deque()
+    def run(self):
+        while True:
+            if len(food_shop.queue) // 2 < len(ticket_shop.queue):  #  TODO: передать ticket и food кассы в класс при инициализации
+                # print(f'{self.name} buying food first')
+                if self.client.food_preference + self.client.drink_preference != 0:
+                    with food_shop.request() as req:
+                        yield req
+                        yield self.env.process(self.buy_snacks())
+                with ticket_shop.request() as req:
+                    yield req
+                    yield self.env.process(self.buy_ticket())
+            else:
+                # print(f'{self.name} buying tickets first')
+                with ticket_shop.request() as req:
+                    yield req
+                    yield self.env.process(self.buy_ticket())
+                if self.client.drink_preference + self.client.food_preference != 0:
+                    with food_shop.request() as req:
+                        yield req
+                        yield self.env.process(self.buy_snacks())
 
-q.append(1)
-q.append(2)
-print(q)
+    def buy_ticket(self):
+        yield self.env.timeout(0.5)
+        print(f'{self.name} bought ticket at {round(self.env.now, 2)}')
 
-print(q.popleft())
-print(q.popleft())
+    def buy_snacks(self):
+        timeout_snacks = self.client.drink_preference + self.client.food_preference
+        yield self.env.timeout(timeout_snacks)
+        print(f'{self.name} bought snacks at {round(self.env.now, 2)}')
+
+
+env = simpy.Environment()
+ticket_shop = simpy.Resource(env, capacity=2)
+food_shop = simpy.Resource(env, capacity=1)
+
+all_clients = 500
+for part_of_clients in range(all_clients // 10):
+    for i in range(50):
+        ClientSim(env, i)
+
+env.run(until=1000)
+
