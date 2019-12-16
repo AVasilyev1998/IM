5336b9f6bd6a3c62e8d0f860087ed7aba4410f6f
dmfananddu <af0706@yandex.ru>
2019-12-16 22:46:59 +0300

    Final

--- simulation.py
+++ simulation.py
@@ -1,41 +1,49 @@
 import simpy
 from client import Client
-import random
+import pickle
 from scheduler import Schedule
 import datetime
+from random import randint
 
 FILMS_COUNT = 7
 HALLS_COUNT = 5
 
+
 class ClientSim(object):
     def __init__(self, env, films_list: list, schedule: Schedule):
         self.client = Client(films_list)
+        self.client.statistics['hall'] = {}
         self.action = env.process(self.run())
         self.env = env
         self.schedule = schedule
+        self.client.statistics['schedule'] = schedule
         self.available_sessions = []
+        
 
     def run(self):
-        if len(food_shop.queue) // 2 < len(ticket_shop.queue):  # TODO: передать ticket и food кассы в класс при инициализации
-            if self.client.food_preference + self.client.drink_preference != 0:
-                with food_shop.request() as req:
-                    yield req
-                    yield self.env.process(self.buy_snacks())
+        print(len(ticket_shop.queue), len(food_shop.queue))
+        if len(ticket_shop.queue) *1.5 <= len(food_shop.queue):  # TODO: передать ticket и food кассы в класс при инициализации
             with ticket_shop.request() as req:
-                yield req
-                yield self.env.process(self.buy_ticket())
-        else:
-            with ticket_shop.request() as req:
+                self.client.statistics['went to cinema'] = self.env.now
                 yield req
                 yield self.env.process(self.buy_ticket())
             if self.client.drink_preference + self.client.food_preference != 0:
                 with food_shop.request() as req:
                     yield req
                     yield self.env.process(self.buy_snacks())
+        else:
+            if self.client.food_preference + self.client.drink_preference != 0:
+                with food_shop.request() as req:
+                    yield req
+                    yield self.env.process(self.buy_snacks())
+            with ticket_shop.request() as req:
+                self.client.statistics['went to cinema'] = self.env.now
+                yield req
+                yield self.env.process(self.buy_ticket())
+
 
     def get_available_sessions(self) -> list:
         now = self.new_now(self.env.now)
-        self.client.statistics['coming time'] = now
         now_minus_30min = now - datetime.timedelta(minutes=30)
         now_plus_1hour = now + datetime.timedelta(hours=1)
         sessions = []
@@ -64,24 +72,24 @@ class ClientSim(object):
 
     def buy_ticket(self):
         # TODO: когда кончаются билеты удалить сессию из расписания (сделать недоступной)
-        now = self.new_now(self.env.now)
         self.available_sessions = self.get_available_sessions()
-        choiced_session = self.choise_session()
-        if choiced_session is not None:
-            choiced_session.free_sits -= 1
-            if choiced_session.free_sits == 0:
-                choiced_session.available = False
+        self.chosen_session = self.choise_session()
+        if self.chosen_session is not None:
+            self.chosen_session.free_sits -= 1
+            if self.chosen_session.free_sits == 0:
+                self.chosen_session.available = False
             self.client.statistics['bought ticket'] = True
-            self.client.statistics['film'] = choiced_session.film_name
-            self.client.statistics['hall'] = choiced_session.hall_name
-            self.client.statistics['ticket buying time'] = now
-            self.client.statistics['session begining time'] = choiced_session.start_time
-            self.client.statistics['spend money'] = choiced_session.ticket_price +\
+            self.client.statistics['film'] = self.chosen_session.film_name
+            self.client.statistics['hall']['capacity'] = self.chosen_session.capacity
+            self.client.statistics['hall']['name'] = self.chosen_session.hall_name
+            now = self.new_now(self.env.now)
+            self.client.statistics['ticket buying time'] = self.env.now
+            self.client.statistics['session begining time'] = self.chosen_session.start_time
+            self.client.statistics['spend money'] = self.chosen_session.ticket_price +\
                 (self.client.food_preference + self.client.drink_preference)*250
             print(f'{self.client.id} bought ticket at {now}')
-            print(f'на фильм {choiced_session.film_name} осталось'
-                f' {choiced_session.free_sits} билетов'
-                )
+            print(f'на фильм {self.chosen_session.film_name} осталось'
+                f' {self.chosen_session.free_sits} билетов')
             yield self.env.timeout(0.5)
         else:
             self.client.statistics['bought ticket'] = False
@@ -97,7 +105,6 @@ class ClientSim(object):
         yield self.env.timeout(timeout_snacks)
 
 
-
 daily_schedule = Schedule(FILMS_COUNT, HALLS_COUNT)
 print(daily_schedule)
 
@@ -107,21 +114,21 @@ films_for_choice = daily_schedule.films_list
 
 
 env = simpy.Environment()
-ticket_shop = simpy.Resource(env, capacity=2)
-food_shop = simpy.Resource(env, capacity=1)
+ticket_shop = simpy.Resource(env, capacity=3)
+food_shop = simpy.Resource(env, capacity=2)
 
 clients = []
-for i in range(3000):
-    client = ClientSim(env, films_for_choice, daily_schedule)
-    clients.append(client)
+def client_creating(env, clients):
+    for i in range(3500):
+        client = ClientSim(env, films_for_choice, daily_schedule)
+        clients.append(client)
+        yield env.timeout(0.5)
+env.process(client_creating(env, clients))
 env.run(until=990)
 
 # Перенос статистики в файл
 statistic = []
 for i in range(clients.__len__()):
     statistic.append(clients[i].client.statistics)
-f = open('sim.txt', 'w')
-f.write(str(statistic))
-f.close()
-
-
+with open('sim.pickle', 'wb') as writer:
+    pickle.dump(statistic, writer)
---
+++ view_statistics.py
@@ -0,0 +1,167 @@
+import pickle
+import matplotlib.pyplot as plt
+
+
+def get_statistics():
+    with open('sim.pickle', 'rb') as reader:
+        data = pickle.load(reader)
+
+    return data
+
+
+def take_films_halls_statistics(statistic: dict) -> dict:
+    statistic_vals = {'films': {}, 'halls': {}, 'average time in queue': 0, 'clients amount': 0, 'revenue': 0, 'gone': 0}
+    for i in range(len(statistic)):
+        # revenue and clients amount
+        if statistic[i]['bought ticket']:
+            statistic_vals["revenue"] += statistic[i]['spend money']
+            statistic_vals['clients amount'] += 1
+        else:
+            statistic_vals['gone'] += 1
+            #  amount of gone people
+        if statistic[i]['film'] is None and statistic[i]['hall'] is None:
+            statistic_vals['gone'] = statistic_vals.setdefault('gone', 0) + 1
+        # amount of bought food
+        if statistic[i]['bought snacks'] is True:
+            statistic_vals['bought food'] = statistic_vals.setdefault('bought food', 0) + 1
+        # time in queue statistic
+        if statistic[i]['bought ticket']:
+            statistic_vals['average time in queue'] += (statistic[i]['ticket buying time'] - statistic[i]['went to cinema'])
+        # halls statistic
+        if len(statistic[i]['hall']) > 0:
+            name = statistic[i]['hall']['name']
+            if statistic_vals['halls'].get(name):
+                statistic_vals['halls'][name]['count'] += 1
+                statistic_vals['halls'][name]['sumTickets'] += statistic[i]['spend money']
+            else:
+                statistic_vals['halls'][name] = dict()
+                statistic_vals['halls'][name]['capacity'] = statistic_vals['halls'][name].setdefault('capacity', statistic[i]['hall']['capacity'])
+                statistic_vals['halls'][name]['count'] = 0
+                statistic_vals['halls'][name]['sumTickets'] = 0
+        if statistic[i]['film'] is not None:
+            statistic_vals['films'][statistic[i]['film']] = statistic_vals['films'].setdefault(
+                str(statistic[i]['film']), 0) + 1
+        maximum = 0
+        for k, v in statistic_vals['films'].items():
+            if v > maximum:
+                pop_film = k
+                maximum = v
+        statistic_vals['most popular film'] = pop_film if pop_film != 0 else None
+    statistic_vals['average time in queue'] = statistic_vals['average time in queue'] // len(statistic)
+    return statistic_vals
+
+
+def bought_tickets_to_film(stat_dict, film_cat='films'):
+    x_films = []
+    y_films = []
+    for k, v in stat_dict[film_cat].items():
+        x_films.append(k)
+        y_films.append(v)
+    plt.bar(x_films, y_films)
+    plt.grid(b='on')
+    plt.yticks()
+    plt.tick_params(axis='y',  # Применяем параметры к обеим осям
+                   which='minor',
+                   length=3,  # Длинна делений
+                   width=1,  # Ширина делений
+                   color='m',  # Цвет делений
+                   pad=10,  # Расстояние между черточкой и ее подписью
+                   labelcolor='r',  # Цвет подписи
+                   left=True)
+    plt.minorticks_on()
+    plt.ylabel('Sold tickets')
+    plt.title('Tickets to films')
+    plt.xticks(rotation=75)
+    plt.show()
+
+
+def halls_graphics(stat_dict, hall_cat='halls'):
+    x_halls = []
+    y_halls = []
+    for k, v in stat_dict[hall_cat].items():
+        x_halls.append(k)
+        y_halls.append(v['count'])
+    plt.pie(y_halls, labels=x_halls, explode=[0.04, 0.04, 0.04, 0.04, 0.04],
+            shadow=True, autopct='%1.1f%%')
+    plt.xticks(rotation=75)
+    plt.title('Halls visiting')
+    plt.show()
+
+
+def halls_profit_by_capacity(stat_dict, capacity_cat='capacity',sum_cat='sumTickets', halls_cat='halls'):
+    x_halls = []
+    y_halls = []
+    for k, v in stat_dict[halls_cat].items():
+        x_halls.append(f'{k} with capacity: {v[capacity_cat]}')
+        y_halls.append(round(v[sum_cat] / v[capacity_cat], 2))
+    plt.bar(x_halls, y_halls)
+    plt.xticks(rotation=75)
+    plt.ylabel('Profit')
+    plt.title('Profit of hall by hall capacity')
+    plt.show()
+    # print(x_halls, y_halls)
+
+
+def time_in_queue(statistics):
+    y_user = []
+    x_user = []
+    for i in range(len(statistics)):
+        if statistics[i]['bought ticket'] is True:
+            y_user.append(int(statistics[i]['ticket buying time'] - statistics[i]['went to cinema']))
+            x_user.append(i)
+    # plt.plot(x_user, y_user)
+    # plt.xlabel('Client')
+    # plt.ylabel('Time in queue')
+    # plt.grid(b='on')
+    # plt.minorticks_on()
+    # plt.title('Dependence betweeen time and number of client')
+    # plt.show()
+    fig, ax = plt.subplots()
+
+    ax.scatter(x_user, y_user,
+               c='b')
+
+    ax.set_facecolor('white')  # цвет области Axes
+    ax.set_title('Time in queue')  # заголовок для Axes
+
+    fig.set_figwidth(8)  # ширина и
+    fig.set_figheight(8)
+    plt.show()
+
+
+def free_sits_on_session(statistics):
+    halls = statistics[0]['schedule'].queues
+    halls_x = []
+    halls_y = []
+    for hall in halls:
+        name = hall[0].hall_name
+
+        for ses in hall:
+            # print(ses)
+            halls_x.append(f'{ses.start_time} | hall {name} | capacity: {ses.capacity}')
+            halls_y.append(ses.free_sits)
+    fig = plt.figure(figsize=(8, 6))
+    plt.bar(halls_x, halls_y)
+    plt.xticks(rotation=90)
+    plt.ylabel('Free sits')
+    plt.title('Free sits on sessions')
+    plt.show()
+
+
+if __name__ == '__main__':
+    statistic_values = get_statistics()
+    stat_values = take_films_halls_statistics(statistic_values)
+    # print(stat_values)
+    print(f'Average time in queue: {stat_values["average time in queue"]}\n'
+          f'Most popular film: {stat_values["most popular film"]}\n'
+          f'Amount of clients: {int(stat_values["clients amount"])}\n'
+          f'Revenue per day: {stat_values["revenue"]}\n'
+          f'Gone clients: {stat_values["gone"]}')
+    bought_tickets_to_film(stat_values)
+    halls_graphics(stat_values)
+    halls_profit_by_capacity(stat_values)
+    time_in_queue(statistic_values)
+    free_sits_on_session(statistic_values)
+
+
+
